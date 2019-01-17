eebd6e1c31d8a63544a6615f69e0672d4bad28b5
skk <skwesiga@gmail.com>
2019-01-17 12:07:38 +0300

    added detail action method

--- src/ComicsBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicsBookGallery/Controllers/ComicBooksController.cs
@@ -8,5 +8,9 @@ namespace ComicsBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public string Detail()
+        {
+            return "Hello from the Comics book controller";
+        }
     }
 }
